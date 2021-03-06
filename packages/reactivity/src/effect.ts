bdcf7fbb2eaeb8b0f1e8870f363b6af62f1891e4
Yokii-ry <775177772@qq.com>
2021-03-07 00:11:38 +0800

    Feat: track和effect进行关联

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,6 @@
 // 第一个参数是回调函数
 // 第二个参数是可选项
+// effect相当于vue2的watcher
 export function effect(fn, options: any = {}) {
     // 需要让effect 变成响应式的api，在数据发生变化时，重新执行effect
     // 所以要创建一个响应式的effect
@@ -15,11 +16,31 @@ export function effect(fn, options: any = {}) {
 
 // 全局变量
 let uid = 0;
+// 存储当前的effect
+let activeEffect = null;
+// effect 执行栈
+let effectStack = [];
+
 // 返回一个函数
 function createReactiveEffect(fn, options) {
     const effect = function reactiveEffect() {
-        // 默认执行时，会执行fn；执行函数时，获取属性值，会调用响应式的get的方法
-        fn();
+        // 将effect加入栈中之前，先判断栈中是否已经包含该effect，避免反复更新属性值，反复调用effect，造成死循环
+        if (!effectStack.includes(effect)) {
+            // 函数可能会发生异常，所以使用try
+            try {
+                // 正在执行的函数当前对应到effect
+                activeEffect = effect;
+                // 若effect回调函数中嵌套了effect，会导致内部effect执行完毕后，再调用属性值到时候，activeEffect还是内部的effct，所以我们使用一个栈来操作
+                // 函数执行时，将effect（的引用地址）推入栈中，并且标识当前fn对应的effect
+                effectStack.push(effect);
+                // 默认执行时，会执行fn；执行函数时，获取属性值，会调用响应式的get的方法
+                return fn();
+            } finally { //执行完毕，使用 finally
+                // 函数执行完毕之后，将effect从栈中弹出，并且标识当前活动effect为栈中最后一个effect
+                effectStack.pop();
+                activeEffect = effectStack[effectStack.length - 1];
+            }
+        }
     }
 
     // 三大特点:  id _isEffect row
@@ -31,6 +52,20 @@ function createReactiveEffect(fn, options) {
     return effect;
 }
 
-export function track() {
-    
+// 非常严重的问题
+// effect(() => {// effect1
+//     state.name; // =>effect1
+//     effect(() => {//effect2
+//         state.age;  // =>effect2
+//     })
+//     state.address;      // =>effect1(如果只使用全局变量，不使用栈的情况下，此处会是effect1)
+// })
+
+
+// 让某个对象中的属性，收集它当前对应的effect
+// 只有取值才可以走这里 
+
+export function track(target, type, key) {
+    // 可以拿到当前的effect
+    activeEffect
 }
